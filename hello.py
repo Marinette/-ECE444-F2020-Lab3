c235a5bb32668e1ec965f92dce74f6c55428858b
Marinette <meggieechen@gmail.com>
2020-09-30 14:01:59 -0400

    updated activity 1

--- hello.py
+++ hello.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 import datetime
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', currentDateTime=datetime.datetime.utcnow())
 @app.route('/user/<name>')
 def user(name):
-    currentTime = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    return render_template('user.html',name=name, currentDateTime=currentTime)
+    return render_template('user.html',name=name, currentDateTime=datetime.datetime.utcnow())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html',currentDateTime=datetime.datetime.utcnow()), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html',currentDateTime=datetime.datetime.utcnow()), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
