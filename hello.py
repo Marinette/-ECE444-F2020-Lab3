891661e85caab7845b7b2156c5298140d49f5d0b
Marinette <meggieechen@gmail.com>
2020-09-30 15:49:59 -0400

    Website takes name as form input and displays following submission

--- hello.py
+++ hello.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 import datetime
+
+#Object Instantiations
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'roccoladocco253'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+#Class Declarations
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+#App Routing
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html', currentDateTime=datetime.datetime.utcnow())
+
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('index.html', currentDateTime=datetime.datetime.utcnow(),form=form, name=name)
+    
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name, currentDateTime=datetime.datetime.utcnow())
@@ -22,5 +41,6 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html',currentDateTime=datetime.datetime.utcnow()), 500
 
+#main
 if __name__ == '__main__':
     app.run(debug=True)
